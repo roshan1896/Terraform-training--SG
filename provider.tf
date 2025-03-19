4de58b23c0d4d5619ff9e152d4deede4afb6bdfc
roshan1896 <152677547+roshan1896@users.noreply.github.com>
2025-03-19 12:31:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,9 +6,8 @@ terraform {
     }
   }
 }
-
 provider "azurerm" {
-features {}
-subscription_id      = "628e279f-926f-43cc-bc30-24777c6b91ef"
-# Configuration options
+  features {}
+  subscription_id      = "628e279f-926f-43cc-bc30-24777c6b91ef"
+  # Configuration options
 }
